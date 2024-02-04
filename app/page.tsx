9edffbffd27117ac2893405f7d173cf159fc623b
mayuresh-c19 <mayuresh1918@gmail.com>
2024-02-04 16:16:44 +0530

    Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-// import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <p>Hello Notion Clone</p>
+    <div>
+      <p>Hello Notion Clone</p>
+      <Button variant={"destructive"} size={"lg"}>
+        Click Me
+      </Button>
+    </div>
   );
 }
